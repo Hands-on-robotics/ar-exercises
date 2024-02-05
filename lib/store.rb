b2554365e5914a9308d5d4a2d7a7f1f8b3c838ac
Hands-on-robotics <handsonevanoff@gmail.com>
2024-02-05 12:04:21 -0800

    complete exercises

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :check_for_apparel
 
+  def check_for_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:Store, "must at least carry mens or womens apparel")
+    end
+  end
 end
